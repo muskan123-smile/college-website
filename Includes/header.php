bee40772e3adacf73c36f88c622f1473901c3c3a
muskan123-smile <goswamimuskan6387@gmail.com>
2026-02-24 23:15:09 -0700

    sagarcollegewebsite

---
+++ Includes/header.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>College Website</title>
+
+<link rel="stylesheet" href="style.css">
+
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+
+
+</style>
+
+</head>
+
+<body>
+
+<!-- TOP BAR -->
+<div class="top-bar">
+    <div class="top-text">
+        ADMISSIONS OPEN FOR 2026-27
+    </div>
+    <a href="#" class="apply-btn">APPLY NOW</a>
+</div>
+
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg bg-white shadow-sm fixed-top" style="top:50px;">
+  <div class="container-fluid px-4">
+
+    <!-- Logo -->
+    <a class="navbar-brand" href="#">
+      <img src="Assets/images/logo.png" width="50px" height="50px">
+    </a>
+
+    
+    <button class="navbar-toggler" type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarMenu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Menu -->
+    <div class="collapse navbar-collapse" id="navbarMenu">
+
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
+            Our Institutions
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Sagar Pharmacy College</a></li>
+            <li><a class="dropdown-item" href="#">Sagar Law College</a></li>
+            <li><a class="dropdown-item" href="#">Sagar Degree College</a></li>
+            <li><a class="dropdown-item" href="#">Sagar International Public School</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item"><a class="nav-link" href="#">Admissions</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Hostel & Student Life</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Placements</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Blogs</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Gallery</a></li>
+
+      </ul>
+
+    </div>
+
+    <!-- Right Side Button  -->
+    <a href="#" class="btn btn-primary ms-lg-5">
+      Admissions Enquiry
+    </a>
+
+  </div>
+</nav>
+
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>College Website</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Bootstrap LINK -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Font Awesom LINK -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- CSS LINK -->
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+
+<!-- HEADER INCLUDE -->
+<?php include 'Includes/header.php'; ?>
+
+
+<!--Slider SECTION -->
+
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Assets/images/slider1.jpeg" class="d-block w-100" style="height:640px; object-fit:cover;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Assets/images/slider2.png" class="d-block w-100"style="height:640px; object-fit:cover;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Assets/images/slider3.jpg" class="d-block w-100" style="height:640px; object-fit:cover;" alt="...">
+    </div>
+     <div class="carousel-item">
+      <img src="Assets/images/slider4.jpg" class="d-block w-100" style="height:640px; object-fit:cover;" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Assets/images/slider5.jpg" class="d-block w-100" style="height:640px; object-fit:cover;" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!--Slider SECTION END -->
+
+
+<!--LOGO SECTION START-->
+
+<section class="affiliation-section pt-5">
+  <div class="container text-center">
+    
+    <h1 class="section-title mb-5">
+      Recognized & Affiliated For Excellence
+    </h1>
+
+    <div class="row justify-content-center align-items-center gx-5 gy-4 ">
+
+      <div class="col-6 col-md-4 col-lg-2 text-center">
+        <img src="Assets/images/aktulogo.jpg" class="affiliation-logo" alt="">
+      </div>
+
+      <div class="col-6 col-md-4 col-lg-2 text-center">
+        <img src="Assets/images/aictelogo.jpg" class="affiliation-logo" alt="">
+      </div>
+
+      <div class="col-6 col-md-4 col-lg-2 text-center">
+        <img src="Assets/images/lucklogo3.jpg" class="affiliation-logo" alt="">
+      </div>
+
+      <div class="col-6 col-md-4 col-lg-2 text-center">
+        <img src="Assets/images/upglogo4.jpg" class="affiliation-logo" alt="">
+      </div>
+
+      <div class="col-6 col-md-4 col-lg-2 text-center">
+        <img src="Assets/images/plogo5.jpg" class="affiliation-logo" alt="">
+      </div>
+
+      <div class="col-6 col-md-4 col-lg-2 text-center">
+        <img src="Assets/images/bhrtlogo6.jpg" class="affiliation-logo" alt="">
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<!--LOGO SECTION END-->
+
+<!--CARD SECTION START-->
+
+<section class="excellence-section pt-5 pb-5">
+  <div class="container text-center">
+
+    <h2 class="section-title mb-5">
+      A Hub of Academic Excellence in Lucknow
+    </h2>
+
+    <div class="row justify-content-center g-5">
+
+      <!-- Card 1 -->
+      <div class="col-md-4">
+        <div class="excellence-card">
+          <div class="icon-box mb-3">
+            <i class="fas fa-graduation-cap"></i>
+          </div>
+          <h4>Multi-Disciplinary Programs</h4>
+          <p>
+            Explore a diverse range of courses across our specialized colleges in Pharmacy, Law, and more, all under one prestigious banner.
+          </p>
+        </div>
+      </div>
+
+      <!-- Card 2 -->
+      <div class="col-md-4">
+        <div class="excellence-card">
+          <div class="icon-box mb-3">
+            <i class="fas fa-flask"></i>
+          </div>
+          <h4>State-of-the-Art Facilities</h4>
+          <p>
+            Learn in a modern environment with advanced laboratories, extensive libraries, and a fully-equipped campus designed for success.
+          </p>
+        </div>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="col-md-4">
+        <div class="excellence-card">
+          <div class="icon-box mb-3">
+            <i class="fas fa-briefcase"></i>
+          </div>
+          <h4>Dedicated Placement Cell</h4>
+          <p>
+            Launch your career with confidence through our robust placement support, industry connections, and exceptional track record.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<!-- Card Section end-->
+
+<!-- Card2 Section Start-->
+
+<section class="vision-section py-5">
+  <div class="container text-center">
+
+    <h1  style="margin-top:80px;" class="section-title mb-5">
+      Our Guiding Vision
+    </h1>
+
+    <div style="margin-top:100px;" class="row justify-content-center g-4">
+
+      <!-- Card 1 -->
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="vision-card">
+          <div class="vision-img">
+            <img src="Assets/images/mam.jpg" alt="">
+          </div>
+          <div class="vision-content">
+            <div class="quote">“</div>
+            <p>
+              Our vision was to build more than a college; it was to create a bedrock of opportunity. We believe that a principled education, rooted in integrity and discipline, is the most powerful tool for shaping a just and prosperous society.
+            </p>
+            <h5>MADHU AGARWAL</h5>
+            <span>CHAIRPERSON (SITM)</span>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 2 -->
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="vision-card">
+          <div class="vision-img">
+            <img src="Assets/images/directorsir.jpeg" alt="">
+          </div>
+          <div class="vision-content">
+            <div class="quote">“</div>
+            <p>
+              At Sagar Institute of Technology and Management (SITM) we instill discipline, dedication,
+               and determination to help students grow into confident, capable professionals. With the support of 
+               parents and guardians, we create an environment.
+            </p>
+            <h5>Dr. J.B. SINGH</h5>
+            <span>DIRECTOR(SITM)</span>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="col-12 col-md-6 col-lg-4">
+        <div class="vision-card">
+          <div class="vision-img">
+            <img src="Assets/images/E1.jpeg" alt="">
+          </div>
+          <div class="vision-content">
+            <div class="quote">“</div>
+            <p>
+              The future demands innovation. 
+              Our focus is on integrating modern teaching methodologies and practical, industry-relevant skills 
+              to ensure our graduates are not
+               just prepared for their first job, but are equipped to lead in their fields for a lifetime.
+            </p>
+            <h5>Shri Parth Upadhyay</h5>
+            <span>Managing Director</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<!-- Card2 Section end-->
+
+<!-- Card3 Section Start-->
+
+<section class="institutions-section py-5">
+  <div class="container text-center">
+    <h1 class="section-title mb-5">Discover Our World-Class Institutions</h1>
+
+    <div class="row custom-gap g-4">
+
+      <!-- Card 1 -->
+      <div class="col-12 col-md-6">
+        <div class="institution-card">
+          <img src="Assets/images/img3.jpeg" alt="Geeta Pharmacy College">
+          <div class="institution-content">
+            <h3>Sagar Pharmacy College</h3>
+            <a href="#">Explore Program →</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 2 -->
+      <div class="col-12 col-md-6">
+        <div class="institution-card">
+          <img src="Assets/images/img2.jpeg" alt="Geeta Law College">
+          <div class="institution-content">
+            <h3>Sagar Law College</h3>
+            <a href="#">Explore Program →</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="col-12 col-md-6">
+        <div class="institution-card">
+          <img src="Assets/images/img1.jpg" alt="Geeta Engineering College">
+          <div class="institution-content">
+            <h3>Sagar Engineering College</h3>
+            <a href="#">Explore Program →</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 4 -->
+      <div class="col-12 col-md-6">
+        <div class="institution-card">
+          <img src="Assets/images/img4.jpeg" alt="Geeta Arts College">
+          <div class="institution-content">
+            <h3>Sagar Arts College</h3>
+            <a href="#">Explore Program →</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+<!-- Slider2 Section Start-->
+
+<section class="alumni-section py-5">
+  <div class="container text-center">
+    <h1 class="section-title mb-5">Success Stories From Our Alumni</h1>
+
+    <div id="alumniCarousel" class="carousel slide" data-bs-ride="carousel">
+
+      <!-- Slides -->
+      <div class="carousel-inner">
+
+        <div class="carousel-item active">
+          <div class="alumni-card mx-auto">
+            <div class="stars">★★★★★</div>
+            <p class="alumni-text">
+              "The foundation in pharmaceutical sciences I received at Geeta College was about more than just academics; it was about learning to innovate."
+            </p>
+            <hr>
+            <h5>Kiyotaka Jafar</h5>
+            <span>GATE Biotechnology Aspirant</span>
+          </div>
+        </div>
+
+        <div class="carousel-item">
+          <div class="alumni-card mx-auto">
+            <div class="stars">★★★★★</div>
+            <p class="alumni-text">
+              "The supportive faculty and modern labs helped me build confidence and prepare for competitive exams successfully."
+            </p>
+            <hr>
+            <h5>Rahul Sharma</h5>
+            <span>Pharma Research Analyst</span>
+          </div>
+        </div>
+
+        <div class="carousel-item">
+          <div class="alumni-card mx-auto">
+            <div class="stars">★★★★★</div>
+            <p class="alumni-text">
+              "The supportive faculty and modern labs helped me build confidence and prepare for competitive exams successfully."
+            </p>
+            <hr>
+            <h5>Rahul Sharma</h5>
+            <span>Pharma Research Analyst</span>
+          </div>
+        </div>
+        
+
+        <div class="carousel-item">
+          <div class="alumni-card mx-auto">
+            <div class="stars">★★★★★</div>
+            <p class="alumni-text">
+              "My journey here shaped my professional growth and prepared me for real-world industry challenges."
+            </p>
+            <hr>
+            <h5>Ananya Singh</h5>
+            <span>Biotech Entrepreneur</span>
+          </div>
+        </div>
+
+      </div>
+
+      
+      <div class="carousel-indicators custom-indicators">
+        <button type="button" data-bs-target="#alumniCarousel" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#alumniCarousel" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#alumniCarousel" data-bs-slide-to="2"></button>
+        <button type="button" data-bs-target="#alumniCarousel" data-bs-slide-to="3"></button>
+      </div>
+
+    </div>
+  </div>
+</section>
+<!-- Slider2 Section end-->
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
